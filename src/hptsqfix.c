4f6419c6b3ae6c458f37c7bb2db747e218622353
Max Chernogor <mche@users.sourceforge.net>
2002-11-05 07:36:11 +0000

    sorting msgbase removed

--- src/hptsqfix.c
+++ src/hptsqfix.c
@@ -50,6 +50,19 @@
 #define fop_wpb (O_CREAT | O_TRUNC | O_RDWR | O_BINARY)
 #define fop_rpb (O_RDWR | O_BINARY)
 
+int tryfind=0;
+int unDelete=0;
+size_t     maxMsgLen;
+
+/*
+typedef struct {
+    time_t msgTime;
+    dword frame_pos;
+}   sortedbase;
+
+sortedbase *SortedBase = NULL;
+*/
+
 int Open_File(char *name, word mode)
 {
     int handle;
@@ -72,10 +85,21 @@ void usage()
     fprintf(stderr, "                 -f  - try to find next header after broken msg\n");
     fprintf(stderr, "                 -e  - 'areafilename' has extension, strip it\n");
     fprintf(stderr, "                 -u  - undelete (restore deleted messages)\n");
-    fprintf(stderr, "                 -s  - sort messages by date written\n");
     exit(-1);
 }
-
+/*
+static int cmp_msgEntry(const void *a, const void *b)
+{
+    const sortedbase* r1 = (sortedbase*)a;
+    const sortedbase* r2 = (sortedbase*)b;
+    
+    if( r1->msgTime > r2->msgTime)
+        return 1;
+    else if( r1->msgTime < r2->msgTime)
+        return -1;
+    return 0;
+}
+*/
 int findhdr(int SqdHandle)
 {
     off_t pos;
@@ -117,28 +141,68 @@ int findhdr(int SqdHandle)
     return rc;
 }
 
-int tryfind=0;
-int unDelete=0;
-int mSort=0;
-
-typedef struct {
-    time_t msgTime;
-    dword frame_pos;
-}   sortedbase;
-
-sortedbase *SortedBase = NULL;
-
-static int cmp_msgEntry(const void *a, const void *b)
+int Checkhdr(int SqdHandle, SQHDR* sqhdr)
 {
-    const sortedbase* r1 = (sortedbase*)a;
-    const sortedbase* r2 = (sortedbase*)b;
+    int stop = 0;
     
-    if( r1->msgTime > r2->msgTime)
-        return 1;
-    else if( r1->msgTime < r2->msgTime)
-        return -1;
-    return 0;
+    if (read_sqhdr(SqdHandle, sqhdr) == 0) {
+        fprintf(stderr, "... end");
+        stop++;
+    }
+    
+    if(stop==0 && sqhdr->frame_length != sqhdr->msg_length)
+        fprintf(stderr, "\nFrame Length != Msg Length %ld:%ld\n",sqhdr->frame_length ,sqhdr->msg_length); 
+    
+    if (stop==0 && sqhdr->id != SQHDRID) {
+        
+        fprintf(stderr, "\nLooks like Message header is damaged\n");
+        
+        if ( tryfind ) {
+            stop = findhdr(SqdHandle);
+            if(stop == 0)
+            stop = -1;
+        }
+        else
+        {
+            fprintf(stderr, "\nYou may want to use -f parameter\n");
+            stop++;
+        }
+    }
+    
+    if (stop==0 && sqhdr->msg_length <= XMSG_SIZE) {
+        
+        fprintf(stderr, "\nMessage body is too short: %ld\n",sqhdr->msg_length);
+        
+        if ( tryfind ) {
+            stop = findhdr(SqdHandle);
+            if(stop == 0)
+            stop = -1;
+        }
+        else
+        {
+            fprintf(stderr, "\nYou may want to use -f parameter\n");
+            stop++;
+        }
+    }
+    
+    if (stop==0 && sqhdr->frame_length > maxMsgLen) {
+        
+        fprintf(stderr, "\nFrame is larger than rest of file\nLooks like Message header is damaged\n");
+        
+        if ( tryfind ) {
+            stop = findhdr(SqdHandle);
+            if(stop == 0)
+            stop = -1;
+        }
+        else
+        {
+            fprintf(stderr, "\nYou may want to use -f parameter\n");
+            stop++;
+        }
+    }
+    return stop;
 }
+
 
 int repair(char *areaName)
 {
@@ -159,8 +223,6 @@ int repair(char *areaName)
     SQHDR      sqhdr;
     XMSG       xmsg;
     SQIDX      sqidx;
-    size_t     maxMsgLen;
-    struct tm  tmdate;
     
     sqd = (char*)malloc(strlen(areaName)+5);
     
@@ -203,300 +265,130 @@ int repair(char *areaName)
     lseek(SqdHandle, 0L, SEEK_SET);
     
     
-    if (lock(SqdHandle, 0, 1) == 0) {
+    if (lock(SqdHandle, 0, 1) != 0) {
+        close(NewSqdHandle);
+        close(NewSqiHandle);
+        close(SqdHandle);
+        free(sqd);
+        free(newsqd);
+        free(newsqi);
+        fprintf(stderr, "\nCan't lock msg base\n");
+        return 0;
+    }
+    read_sqbase(SqdHandle, &sqbase);
+    
+    sqbase.num_msg = 0;
+    sqbase.high_msg = 0;
+    sqbase.skip_msg = 0;
+    sqbase.begin_frame = SQBASE_SIZE;
+    sqbase.last_frame = sqbase.begin_frame;
+    sqbase.free_frame = sqbase.last_free_frame = 0;
+    sqbase.end_frame = sqbase.begin_frame;
+
+    for (stop = 0, i = 1; !stop; i++)
+    {
         
-        read_sqbase(SqdHandle, &sqbase);
-        if(mSort)
+        fprintf(stderr, "Msg %ld", i);
+        
+        stop = Checkhdr(SqdHandle, &sqhdr);
+
+        if(stop == 1)
+            break;
+        if(stop == -1)
         {
-            num_msg = sqbase.num_msg;
-            SortedBase = calloc(sizeof(sortedbase),num_msg);
-            if(!SortedBase)
+            stop = 0; continue;
+        }
+        
+        if (sqhdr.frame_type != FRAME_normal) {
+            if (unDelete != 0 && sqhdr.frame_type == FRAME_free) {
+                sqhdr.frame_type = FRAME_normal;
+                fprintf(stderr, "... free, restoring\n");
+            } 
+            else
             {
-                fprintf(stderr, "\nNot enough memory to sort base\n");
-                mSort = 0;
+                sayFree = 1;
+                fprintf(stderr, "... free\r");
+                lseek(SqdHandle, sqhdr.frame_length, SEEK_CUR);
+                continue;
             }
         }
         
-        sqbase.num_msg = 0;
-        sqbase.high_msg = 0;
-        sqbase.skip_msg = 0;
-        sqbase.begin_frame = SQBASE_SIZE;
-        sqbase.last_frame = sqbase.begin_frame;
-        sqbase.free_frame = sqbase.last_free_frame = 0;
-        sqbase.end_frame = sqbase.begin_frame;
-        write_sqbase(NewSqdHandle, &sqbase);
-        
-        
-        for (stop = 0, i = 1; !stop; i++) {
-            
-            fprintf(stderr, "Msg %ld", i);
-            
-            if (read_sqhdr(SqdHandle, &sqhdr) == 0) {
-                fprintf(stderr, "... end");
-                stop++;
-            }
-            
-            if(sqhdr.frame_length != sqhdr.msg_length)
-                fprintf(stderr, "\nFrame Length != Msg Length %ld:%ld\n",sqhdr.frame_length ,sqhdr.msg_length); 
-            
-            if (stop==0 && sqhdr.id != SQHDRID) {
-                
-                fprintf(stderr, "\nLooks like Message header is damaged\n");
-                
-                if ( tryfind ) {
-                    stop = findhdr(SqdHandle);
-                    continue;
-                }else{
-                    fprintf(stderr, "\nYou may want to use -f parameter\n");
-                    stop++;
-                }
-            }
-            
-            if (stop==0 && sqhdr.msg_length <= XMSG_SIZE) {
-                
-                fprintf(stderr, "\nMessage body is too short: %ld\n",sqhdr.msg_length);
-                
-                if ( tryfind ) {
-                    stop = findhdr(SqdHandle);
-                    continue;
-                }else{
-                    fprintf(stderr, "\nYou may want to use -f parameter\n");
-                    stop++;
-                }
-            }
-            
-            if (stop==0 && sqhdr.frame_length > maxMsgLen) {
-                
-                fprintf(stderr, "\nFrame is larger than rest of file\nLooks like Message header is damaged\n");
-                
-                if ( tryfind ) {
-                    stop = findhdr(SqdHandle);
-                    continue;
-                }else{
-                    fprintf(stderr, "\nYou may want to use -f parameter\n");
-                    stop++;
-                }
-            }
-            
-            if (!stop) {
-                if (sqhdr.frame_type != FRAME_normal) {
-                    if (unDelete != 0 && sqhdr.frame_type == FRAME_free) {
-                        sqhdr.frame_type = FRAME_normal;
-                        fprintf(stderr, "... free, restoring\n");
-                    } else {
-                        sayFree = 1;
-                        fprintf(stderr, "... free\r");
-                        lseek(SqdHandle, sqhdr.frame_length, SEEK_CUR);
-                        continue;
-                    }
-                }
-                
-                read_xmsg(SqdHandle, &xmsg);
-                
-                text = (char*)calloc(sqhdr.frame_length, sizeof(char*));
-                farread(SqdHandle, text, (sqhdr.frame_length - XMSG_SIZE));
-                memcpy(text,text,sqhdr.msg_length);
-                frame_length       = sqhdr.frame_length;
-                sqhdr.frame_length = sqhdr.msg_length;
-                if (firstmsg) {
-                    sqhdr.prev_frame = 0;
-                    firstmsg=0;
-                } else {
-                    sqhdr.prev_frame = sqbase.last_frame;
-                } /* endif */
-                
-                sqhdr.next_frame = tell(NewSqdHandle) + SQHDR_SIZE + sqhdr.msg_length;
-                
-                sqbase.last_frame = tell(NewSqdHandle);
-                sqidx.ofs = sqbase.last_frame;
-                
-                if(mSort)
-                {
-                    if(num_msg == sqbase.num_msg)
-                    {
-                        num_msg = sqbase.num_msg+1;
-                        SortedBase = realloc(SortedBase,num_msg*sizeof(sortedbase));
-                        if(!SortedBase)
-                        {
-                            fprintf(stderr, "\nNot enough memory to sort base\n");
-                            mSort = 0;
-                        }
-                    }
-                    DosDate_to_TmDate((union stamp_combo *)&xmsg.date_written, &tmdate);
-                    SortedBase[sqbase.num_msg].frame_pos = sqbase.last_frame;
-                    SortedBase[sqbase.num_msg].msgTime   = mktime(&tmdate);
-                }
-                
-                write_sqhdr(NewSqdHandle, &sqhdr);
-                write_xmsg(NewSqdHandle, &xmsg);
-                farwrite(NewSqdHandle, text, (sqhdr.msg_length - XMSG_SIZE));
-                
-                sqbase.end_frame = tell(NewSqdHandle);
-                
-                sqidx.hash = SquishHash(xmsg.to);
-                if (xmsg.attr & MSGREAD) {
-                    sqidx.hash |= (dword) 0x80000000L;
-                }
-                sqidx.umsgid = sqbase.num_msg+1;
-                
-                write_sqidx(NewSqiHandle, &sqidx, 1);
-                free(text);
-                
-                sqbase.num_msg++;
-                sqbase.high_msg = sqbase.num_msg;
-                saved++;
-                if (sayFree) {
-                    fprintf(stderr, "        ");
-                    sayFree=0;
-                }
-                fprintf(stderr, "\r");
-                
-                maxMsgLen -= frame_length;
-            }
-       } /* endfor */
-       
-       fprintf(stderr, "\n%ld messages read\n", i-2);
-       
-       lseek(NewSqiHandle, SQIDX_SIZE, SEEK_END);
-       read_sqidx(NewSqiHandle, &sqidx, 1);
-       lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
-       read_sqhdr(NewSqdHandle, &sqhdr);
-       sqhdr.next_frame = 0;
-       lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
-       write_sqhdr(NewSqdHandle, &sqhdr);
-       lseek(NewSqdHandle, 0L, SEEK_SET);
-       write_sqbase(NewSqdHandle, &sqbase);
-       
-       unlock(SqdHandle, 0, 1);
-       
-       close(NewSqdHandle);
-       close(NewSqiHandle);
-       close(SqdHandle);
-       
-       if(mSort)
-       {
-           num_msg = sqbase.num_msg;           
-           
-           fprintf(stderr, "\nSorting msgbase...\n");
-           
-           qsort( (void*)SortedBase, num_msg, sizeof(sortedbase), cmp_msgEntry ); 
-           
-           sprintf(sqd, "%s.tmd", areaName );
-           SqdHandle = Open_File(sqd, fop_rpb);
-           if (SqdHandle == -1) {
-               free(sqd);
-               free(newsqd);
-               free(newsqi);
-               return 0;
-           } /* endif */
-           
-           sprintf(newsqd, "%s.tsd", areaName);
-           sprintf(newsqi, "%s.tsi", areaName);
-           
-           NewSqdHandle = Open_File(newsqd, fop_wpb);
-           if (NewSqdHandle == -1) {
-               free(sqd);
-               free(newsqd);
-               free(newsqi);
-               close(SqdHandle);
-               return 0;
-           } /* endif */
-           NewSqiHandle = Open_File(newsqi, fop_wpb);
-           if (NewSqiHandle == -1) {
-               free(sqd);
-               free(newsqd);
-               free(newsqi);
-               close(NewSqdHandle);
-               close(SqdHandle);
-               return 0;
-           } /* endif */
-           
-           lseek(SqdHandle, 0L, SEEK_END);
-           maxMsgLen = tell(SqdHandle) - SQBASE_SIZE - SQHDR_SIZE;
-           lseek(SqdHandle, 0L, SEEK_SET);
-           
-           
-           if (lock(SqdHandle, 0, 1) == 0) {
-               firstmsg=0;
-               read_sqbase(SqdHandle, &sqbase);
-               sqbase.num_msg = 0;
-               sqbase.high_msg = 0;
-               sqbase.skip_msg = 0;
-               sqbase.begin_frame = SQBASE_SIZE;
-               sqbase.last_frame = sqbase.begin_frame;
-               sqbase.free_frame = sqbase.last_free_frame = 0;
-               sqbase.end_frame = sqbase.begin_frame;
-               write_sqbase(NewSqdHandle, &sqbase);
-               
-               for (i = 0; i < num_msg; i++) {
-                   
-                   lseek(SqdHandle, SortedBase[i].frame_pos , SEEK_SET);
+        read_xmsg(SqdHandle, &xmsg);
+        xmsg.attr = 0;
+        xmsg.replyto = 0;
+        memset(xmsg.replies, '\000', sizeof(UMSGID)*MAX_REPLY);	    
 
-                   if (read_sqhdr(SqdHandle, &sqhdr) == 0) {
-                       fprintf(stderr, "... end");
-                       break;
-                   }
-                   read_xmsg(SqdHandle, &xmsg);
-                   text = (char*)calloc(sqhdr.frame_length, sizeof(char*));
-                   farread(SqdHandle, text, (sqhdr.frame_length - XMSG_SIZE));
-                   memcpy(text,text,sqhdr.msg_length);
-                   if (firstmsg) {
-                       sqhdr.prev_frame = 0;
-                       firstmsg=0;
-                   } else {
-                       sqhdr.prev_frame = sqbase.last_frame;
-                   } /* endif */
-                   
-                   sqhdr.next_frame = tell(NewSqdHandle) + SQHDR_SIZE + sqhdr.msg_length;
-                   sqbase.last_frame = tell(NewSqdHandle);
-                   sqidx.ofs = sqbase.last_frame;
-                   write_sqhdr(NewSqdHandle, &sqhdr);
-                   write_xmsg(NewSqdHandle, &xmsg);
-                   farwrite(NewSqdHandle, text, (sqhdr.msg_length - XMSG_SIZE));
-                   sqbase.end_frame = tell(NewSqdHandle);
-                   sqidx.hash = SquishHash(xmsg.to);
-                   if (xmsg.attr & MSGREAD) {
-                       sqidx.hash |= (dword) 0x80000000L;
-                   }
-                   sqidx.umsgid = sqbase.num_msg+1;
-                   write_sqidx(NewSqiHandle, &sqidx, 1);
-                   free(text);
-                   sqbase.num_msg++;
-                   sqbase.high_msg = sqbase.num_msg;
-                   maxMsgLen -= sqhdr.frame_length;
-               }
-               lseek(NewSqiHandle, SQIDX_SIZE, SEEK_END);
-               read_sqidx(NewSqiHandle, &sqidx, 1);
-               lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
-               read_sqhdr(NewSqdHandle, &sqhdr);
-               sqhdr.next_frame = 0;
-               lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
-               write_sqhdr(NewSqdHandle, &sqhdr);
-               lseek(NewSqdHandle, 0L, SEEK_SET);
-               write_sqbase(NewSqdHandle, &sqbase);
-               
-               unlock(SqdHandle, 0, 1);
-               
-               close(NewSqdHandle);
-               close(NewSqiHandle);
-               close(SqdHandle);
-               
-           } 
-       }
-   } else {
-       close(NewSqdHandle);
-       close(NewSqiHandle);
-       close(SqdHandle);
+        text = (char*)calloc(sqhdr.frame_length, sizeof(char*));
+        farread(SqdHandle, text, (sqhdr.frame_length - XMSG_SIZE));
+        memcpy(text,text,sqhdr.msg_length);
+        frame_length       = sqhdr.frame_length;
+        sqhdr.frame_length = sqhdr.msg_length;
+        if (firstmsg) {
+            sqhdr.prev_frame = 0;
+            firstmsg=0;
+        } 
+        else 
+        {
+            sqhdr.prev_frame = sqbase.last_frame;
+        } 
+        
+        sqhdr.next_frame = tell(NewSqdHandle) + SQHDR_SIZE + sqhdr.msg_length;
+        
+        sqbase.last_frame = tell(NewSqdHandle);
+        sqidx.ofs = sqbase.last_frame;
+        
+        write_sqhdr(NewSqdHandle, &sqhdr);
+        write_xmsg(NewSqdHandle, &xmsg);
+        farwrite(NewSqdHandle, text, (sqhdr.msg_length - XMSG_SIZE));
+        
+        sqbase.end_frame = tell(NewSqdHandle);
+        
+        sqidx.hash = SquishHash(xmsg.to);
+        if (xmsg.attr & MSGREAD) {
+            sqidx.hash |= (dword) 0x80000000L;
+        }
+        sqidx.umsgid = sqbase.num_msg+1;
+        
+        write_sqidx(NewSqiHandle, &sqidx, 1);
+        free(text);
+        
+        sqbase.num_msg++;
+        sqbase.high_msg = sqbase.num_msg;
+        saved++;
+        if (sayFree) {
+            fprintf(stderr, "        ");
+            sayFree=0;
+        }
+        fprintf(stderr, "\r");
+        
+        maxMsgLen -= frame_length;
+    } 
+
+    fprintf(stderr, "\n%ld messages read\n", i-2);
+    
+    lseek(NewSqiHandle, SQIDX_SIZE, SEEK_END);
+    read_sqidx(NewSqiHandle, &sqidx, 1);
+    lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
+    read_sqhdr(NewSqdHandle, &sqhdr);
+    sqhdr.next_frame = 0;
+    lseek(NewSqdHandle, sqidx.ofs, SEEK_SET);
+    write_sqhdr(NewSqdHandle, &sqhdr);
+    lseek(NewSqdHandle, 0L, SEEK_SET);
+    write_sqbase(NewSqdHandle, &sqbase);
+    
+    unlock(SqdHandle, 0, 1);
+    
+    close(NewSqdHandle);
+    close(NewSqiHandle);
+    close(SqdHandle);
+
+       free(sqd);
+       free(newsqd);
+       free(newsqi);
        
-   } /* endif */
-   
-   free(sqd);
-   free(newsqd);
-   free(newsqi);
-   
-   fprintf(stderr, "%ld messages saved\n", saved);
-   
-   return 0;
+       fprintf(stderr, "%ld messages saved\n", saved);
+       
+       return 0;
 }
 
 
@@ -515,8 +407,6 @@ int main(int argc, char *argv[])
                 stripExt = 1;
             } else if (stricmp(argv[i], "-u")==0) {
                 unDelete = 1;
-            } else if (stricmp(argv[i], "-s")==0) {
-                mSort = 1;
             } else {
                 usage();
             }
