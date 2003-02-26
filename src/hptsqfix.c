f3f1170c0c991b71c7f506df3d22562c87921f5b
Stas Degteff <stas_degteff@users.sourceforge.net>
2003-02-26 05:56:15 +0000

    Use compiler-specific defines from compiler.h

--- src/hptsqfix.c
+++ src/hptsqfix.c
@@ -3,49 +3,57 @@
 *      made by Serguei Revtov 2:5021/19.1
 *      from hptUtil by Fedor Lizunkov 2:5020/960@Fidonet
 *
-* This file is part of HPT.
+* This file is part of HPTSQFIX, part of The HUSKY Fidonet Software project.
+* About HUSKY see http://husky.sourceforge.net (http://husky.sf.net)
 *
-* HPT is free software; you can redistribute it and/or modify it
+* HPTSQFIX is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
 * Free Software Foundation; either version 2, or (at your option) any
 * later version.
 *
-* HPT is distributed in the hope that it will be useful, but
+* HPTSQFIX is distributed in the hope that it will be useful, but
 * WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with HPT; see the file COPYING.  If not, write to the Free
+* along with HPTSQFIX; see the file COPYING.  If not, write to the Free
 * Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+* See also http://www.gnu.org
 *****************************************************************************
 */
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
-#if !defined(UNIX) && !defined(SASC)
-#include <io.h>
-#endif
-
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#if !defined(UNIX) && !defined(SASC)
+/* compiler.h */
+#include <smapi/compiler.h>
+
+#if defined(HAS_IO_H)
+#include <io.h>
+#endif
+
+#if defined(HAS_SHARE_H)
 #include <share.h>
 #endif
-#if !(defined(_MSC_VER) && (_MSC_VER >= 1200))
+
+#if defined(HAS_UNISTD_H)
 #include <unistd.h>
 #endif
 
+/* smapi */
 #include <smapi/prog.h>
 #include <smapi/msgapi.h>
-#include <squish.h>
+
+/* sqpack */
+#include "../h/squish.h"
 
 #define VERSION	"1.9.0-cur"
-#include "cvsdate.h"
+#include "../h/cvsdate.h"
 
 #define fop_wpb (O_CREAT | O_TRUNC | O_RDWR | O_BINARY)
 #define fop_rpb (O_RDWR | O_BINARY)
@@ -67,7 +75,7 @@ int Open_File(char *name, word mode)
 {
     int handle;
     
-#ifdef UNIX
+#ifdef __UNIX__
     handle = sopen(name, mode, SH_DENYNONE, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 #else
     handle = sopen(name, mode, SH_DENYNONE, S_IREAD | S_IWRITE);
