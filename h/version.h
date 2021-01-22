7181ff228870988ebf496bbdcaefee6d08404c3e
Michael Dukelsky <dukelsky@users.noreply.github.com>
2021-01-22 18:44:55 +0300

    uncrustify sources

--- h/version.h
+++ h/version.h
@@ -4,13 +4,12 @@
 #define _VERSION_H
 
 #include "cvsdate.h"
-
 /* basic version number */
 #define VER_MAJOR 1
 #define VER_MINOR 9
 #define VER_PATCH 0
 #define VER_BRANCH BRANCH_CURRENT
 
-extern char      *versionStr;
+extern char * versionStr;
 
 #endif
